4a47fd694db46173399cd42ab391e740c3ae8041
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-04-01 00:49:09 +0100

    [OI] update integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
+describe group('mongodb') do
+  it { should exist }
+end
+
+describe user('mongodb') do
+  it { should exist }
+  it { should belong_to_group 'mongodb' }
+end
+
 describe package('mongodb-org') do
   it { should be_installed }
+end
+
+describe file('/var/lib/mongodb') do
+  it { should be_directory }
+  it { should be_mode 755 }
+  it { should be_owned_by 'mongodb' }
+end
+
+describe file('/etc/mongod.conf') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'root' }
 end
 
 describe service('mongod') do
